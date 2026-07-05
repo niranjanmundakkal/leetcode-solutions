52ea95767e362994fe11042fac1ccb37f88fcc34
NIRANJAN M <niranjanmcvn10b@gmail.com>
2026-07-05 11:55:16 +0530

    Add files via upload

---
+++ Easy/217_containsduplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        s=set(nums) 
+        if len(s)==len(nums):
+            return False
+        else:
+            return True
+           
