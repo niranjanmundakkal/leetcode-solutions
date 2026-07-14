21d03e29b97f10f37e56751be1c2416b15921807
NIRANJAN M <niranjanmcvn10b@gmail.com>
2026-07-14 23:52:16 +0530

    Add files via upload

---
+++ Medium/739. Daily Temperatures.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        n=len(temperatures)
+        stack=[]
+        ans=[0] * n
+        for i in range(n):
+            while stack and temperatures[i]>temperatures[stack[-1]]:
+                idx=stack.pop()
+                ans[idx]=i-idx
+            stack.append(i)
+        return ans
